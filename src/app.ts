89622a9a7d056e0597ab2e161ed8c49aa8925781
Odair Cesar Ferreira <odairferreira97@gmail.com>
2023-05-03 21:32:17 -0300

    CORREÇÕES: Adicionando cors ao servidor

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import "reflect-metadata"
 import express = require("express")
 import { json } from "body-parser"
 import { routes } from "./routes"
+import cors = require('cors')
 
 const app = express()
 
+app.use(cors()) 
 app.use(json())
 app.use(routes)
 
